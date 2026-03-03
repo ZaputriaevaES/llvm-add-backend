f163368453757624a64cbfa31ebb5a711dcca758
ZaputriaevaES <zaputriaeva.es@phystech.edu>
2026-03-03 13:07:00 +0300

    fix my arch

---
+++ llvm/lib/Target/EZapArch/MCTargetDesc/EZapArchMCTargetDesc.cpp
@@ -0,0 +1,7 @@
+#include "llvm/MC/TargetRegistry.h"
+#include "TargetInfo/EZapArchTargetInfo.h"
+
+using namespace llvm;
+
+extern "C" LLVM_EXTERNAL_VISIBILITY void LLVMInitializeEZapArchTargetMC() {
+}
